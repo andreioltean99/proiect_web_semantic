fcf01cbc41f60c029e858f15eb17713be31ee950
Andrei Oltean <olteanandreicj@gmail.com>
2021-04-24 22:35:47 +0300

    citireBDProiecte+preluare+afisare

--- index.js
+++ index.js
@@ -9,16 +9,25 @@ btn1.addEventListener("click", function(){
   })
   .then(function (response) {
     document.getElementById('side-nav').style.display='block'; // se afiseaza sectiunea cu toate proiectele preluate din BD
-    console.log(response);
+
+var stringResponse = response.data;
+
+var splitstring = stringResponse.split('|')
+for(let i=0; i<splitstring.length-1; i++){
+  var node = document.createElement("LI");                 
+var textnode = document.createTextNode(splitstring[i]);       
+node.appendChild(textnode); 
+node.setAttribute('id',`${splitstring[i]+'_prj_id'}`);
+node.setAttribute('onclick',`redirect('${splitstring[i]}')`);          
+document.getElementById("projects-list").appendChild(node); 
+
+}
   })
   .catch(function (error) {
     console.log(error);
   });
 })
 //-----------------------------------------
-btnInsert.addEventListener('focus', function(){
-    console.log("fortmularul este trimis");
-})
 
 function deleteTask(taskNumber){
       // Se efectueaza cerea catre server
@@ -40,6 +49,10 @@ btnInsert.addEventListener('click', function(event){
 btnInsert.addEventListener('mouseover', function(){
     console.log("insereaza date");
 })
+
+function redirect(projectName){
+ console.log(projectName);
+}
 
 
  
